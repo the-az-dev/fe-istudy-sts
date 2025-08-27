6e2e6e39e878d2efaaa9c04525b428eb4b3bafb9
azik <azdrobylko@gmail.com>
2025-08-27 16:19:27 +0300

    [FIX] Fixing build options

--- src/shared/app.routes.ts
+++ src/shared/app.routes.ts
@@ -1,42 +1,38 @@
-import {
-  createRouter,
-  createWebHistory,
-  type RouteRecordRaw,
-} from "vue-router";
+import {createRouter, createWebHistory, type RouteRecordRaw,} from "vue-router";
 import TeacherPage from "../features/teacher/presentations/pages/TeacherPage.vue";
 import AuthPage from "../features/auth/presentations/pages/AuthPage.vue";
 import TeacherDashboard from "../features/dashboard/presentations/pages/TeacherDashboard.vue";
 
 const routes: Readonly<RouteRecordRaw[]> = [
-  { path: "", component: TeacherDashboard, meta: { requiresAuth: true } },
-  { name: "login", path: "/auth", component: AuthPage, meta: { requiresAuth: false } },
-  {
-    path: "/teacher",
-    component: TeacherPage,
-    children: [
-      {
-        path: "",
-        component: TeacherDashboard,
-        meta: { requiresAuth: true },
-      },
-    ],
-    meta: { requiresAuth: true },
-  },
+    {path: "", component: TeacherDashboard, meta: {requiresAuth: true}},
+    {name: "login", path: "/auth", component: AuthPage, meta: {requiresAuth: false}},
+    {
+        path: "/teacher",
+        component: TeacherPage,
+        children: [
+            {
+                path: "",
+                component: TeacherDashboard,
+                meta: {requiresAuth: true},
+            },
+        ],
+        meta: {requiresAuth: true},
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory('/your-repo-name/'),
+    routes,
 });
 
 router.beforeEach((to, _, next) => {
-  const isAuthenticated = !!localStorage.getItem("token");
+    const isAuthenticated = !!localStorage.getItem("token");
 
-  if (to.meta.requiresAuth && !isAuthenticated) {
-    next({ name: "login" });
-  } else {
-    next();
-  }
+    if (to.meta.requiresAuth && !isAuthenticated) {
+        next({name: "login"});
+    } else {
+        next();
+    }
 });
 
 // router.onError((error) => {
