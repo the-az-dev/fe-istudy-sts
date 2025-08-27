bfde1afee471f33c877d1e0e6ce122d41f18456e
azik <azdrobylko@gmail.com>
2025-08-27 15:39:07 +0300

    [FIX] Fixing major bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from '@tailwindcss/vite';
-import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  base: "",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@app": path.resolve(__dirname, "src/app"),
-      "@shared": path.resolve(__dirname, "src/shared"),
-      "@features": path.resolve(__dirname, "src/features"),
+    plugins: [vue(), tailwindcss()],
+    base: "",
+    resolve: {
+        alias: {
+            "@": "/src",
+            "@app": "/src/app",
+            "@shared": "/src/shared",
+            "@features": "/src/features",
+        },
     },
-  },
 });
