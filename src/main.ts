79453c0576e697c09407b34652a303b01b035c06
azik <azdrobylko@gmail.com>
2025-08-30 18:58:06 +0300

    [UPDATE] Adding Pinia like a state-manager

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import {createApp} from "vue";
 import "./shared/styles/main.css";
 import App from "./app/App.vue";
 import router from "./shared/app.routes";
+import {createPinia} from "pinia";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+    .use(router)
+    .use(createPinia())
+    .mount("#app");
