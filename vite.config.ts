e5258f191c5a2f3f458aba584de0729525c76b8d
azik <azdrobylko@gmail.com>
2025-08-30 18:40:20 +0300

    [FIX] Fixing build options. Adding new params for buildings. Build destinations setuped.

--- vite.config.ts
+++ vite.config.ts
@@ -4,41 +4,27 @@ import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [tsconfigPaths(), vue(), tailwindcss()],
-    base: "/fe-istudy-sts/",
-    resolve: {
-        alias: {
-            "@app": "/src/app",
-            "@shared": "/src/shared",
-            "@features": "/src/features",
+export default defineConfig(() => {
+    return {
+        plugins: [tsconfigPaths(), vue(), tailwindcss()],
+        base: process.env.DEST === 'gh' ? "/fe-istudy-sts/" : "",
+        resolve: {
+            alias: {
+                "@app": "/src/app",
+                "@shared": "/src/shared",
+                "@features": "/src/features",
+            },
         },
-    },
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-        // Генерувати source maps для дебагу
-        sourcemap: true,
-        // Оптимізація чанків
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vendor: ['vue', 'vue-router']
+        build: {
+            assetsDir: 'public',
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        vendor: ['vue', 'vue-router'],
+                        pinia: ['pinia']
+                    }
                 }
             }
         }
-    }
+    };
 });
-
-//
-// function getPathsFromTsConfig() {
-//     const tsconfig_s = fs
-//         .readFileSync('./tsconfig.json', 'utf-8')
-//         .replace(/\/\/.*$/gm, '') // Removing comments
-//     const tsconfig = JSON.parse(tsconfig_s)
-//     const aliases = {}
-//     for (const [key, value] of Object.entries(tsconfig.compilerOptions.paths)) {
-//         aliases[key] = path.resolve(__dirname, value[0])
-//     }
-//     return aliases
-// }
