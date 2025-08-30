621c223d30ffafb2d5c3b0972e7b05c427e8d26c
azik <azdrobylko@gmail.com>
2025-08-30 23:23:26 +0300

    [UPDATE] Adding new rules for `process.env` logic at `.vue` files

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,19 @@ export default defineConfig(() => {
         base: process.env.DEST === 'gh' ? "/fe-istudy-sts/" : "",
         resolve: {
             alias: {
+                "@": "/",
                 "@app": "/src/app",
                 "@shared": "/src/shared",
+                "@stores": "/src/shared/stores/",
+                "@components": "/src/shared/components",
+                "@assets": "/src/shared/assets",
+                "@utils": "/src/shared/utils",
+                "@styles": "/src/shared/styles",
                 "@features": "/src/features",
             },
+        },
+        define: {
+            'process.env': process.env
         },
         build: {
             assetsDir: 'public',
